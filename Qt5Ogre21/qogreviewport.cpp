2d77100b9a0624f7e64b20f68347e9121f8a5ab4
Arthur Brainville <ybalrid@ybalrid.info>
2016-11-20 15:50:26 +0100

    add comments

--- Qt5Ogre21/qogreviewport.cpp
+++ Qt5Ogre21/qogreviewport.cpp
@@ -7,6 +7,7 @@ QOgreViewport::QOgreViewport(size_t SceneManagerIndex, QWidget *parent)
       smgrIndex{SceneManagerIndex},
       Window{nullptr}
 {
+    //We need QtOgre21 to exist
     if(!QtOgre21::instance())
         throw std::runtime_error("Cannot create QOgreViewport widgets if QtOgre21 is not initialized!");
 
@@ -22,18 +23,25 @@ QOgreViewport::QOgreViewport(size_t SceneManagerIndex, QWidget *parent)
     misc["FSAA"] = std::to_string(QtOgre21::instance()->getAALevel());
     misc["vsync"] = "false";
     misc["externalWindowHandle"] = std::to_string(winId());
+
+    //This permit to handle some weirdness on window creations
     QtOgre21::instance()->willCreateWindowHint();
+
+    //When using the GL3+ renderer, by default the RenderWindow creation create a GL Context.
+    //Ogre expect to use only one GL Context, so subsequent window need to use the context of the 1st one.
     if(QtOgre21::instance()->getAPI() == QtOgre21::RenderAPI::OpenGL)
+        //Get the context. This pointer is nullptr if there's no context created yet
         if(auto context = QtOgre21::instance()->getContext())
+            //At this point, everything will go bad if this window don't use the same GL context
             misc["externalGLContext"] = std::to_string(size_t(context));
 
     //Create the RenderWindow
     Window = Ogre::Root::getSingleton().createRenderWindow(std::string("Window") + std::to_string(windowNumber++), width(), height(), false, &misc);
 
-    //We have to do this callback. This initialize the scene and the compositor for the rendering, and return a bunch of pointers to usefull stuff as a tuple
+    //We have to do this callback. This initialize the scene and the compositor for the rendering, and return a bunch of pointers in a tuple
     auto result = QtOgre21::instance()->WidgetCreatedCallback(Window, SceneManagerIndex);
 
-    //Extract the "bunch of usefull pointers"
+    //Extract the "bunch of usefull pointers" from the tuple
     SceneManager = std::get<0>(result);
     Camera = std::get<1>(result);
     Workspace = std::get<2>(result);
@@ -42,11 +50,9 @@ QOgreViewport::QOgreViewport(size_t SceneManagerIndex, QWidget *parent)
     //Configure the camera to a state where it will be able to draw something...
     Camera->setNearClipDistance(0.1f);
     Camera->setFarClipDistance(500);
-    Camera->setPosition(0,0,10);
-    Camera->lookAt(0,0,0);
+    Camera->setPosition(0, 0, 10);
+    Camera->lookAt(0, 0, 0);
     Camera->setAutoAspectRatio(true);
-
-
 }
 
 QOgreViewport::~QOgreViewport()
